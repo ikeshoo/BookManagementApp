8f451eb907c6706ad54f76420d60d756db5407d0
ikeshoo <jyfk776@gmail.com>
2022-03-03 23:42:34 +0900

    TopBarを表示

--- app/src/main/java/com/wings/android/bookmanagementapp/view/ui/MainActivity.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/view/ui/MainActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.wings.android.bookmanagementapp.view.theme.BookManagementAppTheme
@@ -16,29 +22,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BookManagementAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            BookManagementApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-    "textCommit"
+fun BookManagementApp() {
+    BookManagementAppTheme {
+        Scaffold(
+            topBar = { TopBar() },
+            bottomBar = { }
+        ) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
+    CenterAlignedTopAppBar(
+        title = { Text(text = "タイトル") },
+        scrollBehavior = scrollBehavior
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BookManagementAppTheme {
-        Greeting("Android")
-    }
+fun AppPreview() {
+    BookManagementApp()
 }
