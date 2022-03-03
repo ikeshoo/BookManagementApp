7d8f1043e98e83b0f1660c3233851880dcb55926
Sho Ikegami <s.ikegami@caraquri.com>
2022-03-03 22:44:05 +0900

    Initial commit

---
+++ app/src/main/java/com/wings/android/bookmanagementapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.wings.android.bookmanagementapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.wings.android.bookmanagementapp.ui.theme.BookManagementAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BookManagementAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BookManagementAppTheme {
+        Greeting("Android")
+    }
+}
