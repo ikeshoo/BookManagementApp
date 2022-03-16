7a43f832c555c4dd08091e8c527daf4750eee614
ikeshoo <jyfk776@gmail.com>
2022-03-16 23:54:03 +0900

    NavHostの追加

--- app/src/main/java/com/wings/android/bookmanagementapp/view/model/BottomBarItem.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/view/model/BottomBarItem.kt
@@ -8,15 +8,19 @@ sealed class BottomBarItem(
     @StringRes
     val textRes: Int,
     @DrawableRes
-    val iconRes: Int
+    val iconRes: Int,
+    val screenRoute: String
 ) {
     object BookShelf : BottomBarItem(
         R.string.bottom_bar_shelf,
-        R.drawable.ic_baseline_menu_book_24
+        R.drawable.ic_baseline_menu_book_24,
+        "bookShelf"
     )
+
     object Search : BottomBarItem(
         R.string.bottom_bar_search,
-        R.drawable.ic_baseline_search_24
+        R.drawable.ic_baseline_search_24,
+        "search"
     )
 
     // ToDo: もう一つくらい追加する？
---
+++ app/src/main/java/com/wings/android/bookmanagementapp/view/ui/BookShelfNavHost.kt
@@ -0,0 +1,31 @@
+package com.wings.android.bookmanagementapp.view.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.wings.android.bookmanagementapp.view.model.BottomBarItem
+
+@Composable
+fun BookShelfNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarItem.BookShelf.screenRoute,
+        modifier = modifier
+    ) {
+        // ToDo: 画面の中身は暫定。正しいレイアウトを作成する。
+        composable(BottomBarItem.BookShelf.screenRoute) {
+            Text(text = stringResource(id = BottomBarItem.BookShelf.textRes))
+        }
+
+        composable(BottomBarItem.Search.screenRoute) {
+            Text(text = stringResource(id = BottomBarItem.Search.textRes))
+        }
+    }
+}
