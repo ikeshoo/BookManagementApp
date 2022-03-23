b547dc49f3226995eb691103db350519745b3b06
ikeshoo <jyfk776@gmail.com>
2022-03-23 23:52:07 +0900

    検索画面にEditText追加

--- app/src/main/java/com/wings/android/bookmanagementapp/view/ui/search/SearchScreen.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/view/ui/search/SearchScreen.kt
@@ -1,17 +1,54 @@
 package com.wings.android.bookmanagementapp.view.ui.search
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 // ToDo: レイアウトは暫定。正しいものへ差し替える。
 @Composable
 fun SearchScreen() {
-    Text(text = "検索")
+    SearchView()
+}
+
+@Composable
+fun SearchView(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("Search") }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = { text = "onSearched" }
+        ),
+        singleLine = true,
+        maxLines = 1,
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewSearchScreen() {
     SearchScreen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSearchView() {
+    SearchView()
 }
