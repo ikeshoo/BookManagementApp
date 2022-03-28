03bbbc214aa1629eba1f9c08ef95cf730a4aa8fb
ikeshoo <jyfk776@gmail.com>
2022-03-28 23:19:23 +0900

    SearchResponseの修正

--- app/src/main/java/com/wings/android/bookmanagementapp/data/source/remote/entity/SearchResult.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/data/source/remote/entity/SearchResult.kt
@@ -1,10 +1,26 @@
 package com.wings.android.bookmanagementapp.data.source.remote.entity
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchResult(
-    @SerialName("Item")
-    val item: SearchResultDetail
+    val author: String,
+    val authorKana: String,
+    val isbn: String,
+    val itemCaption: String,
+    val itemUrl: String,
+    val largeImageUrl: String,
+    val mediumImageUrl: String,
+    val publisherName: String,
+    val reviewAverage: String,
+    val reviewCount: Int,
+    val salesDate: String,
+    val seriesName: String,
+    val seriesNameKana: String,
+    val size: String,
+    val smallImageUrl: String,
+    val subTitle: String,
+    val subTitleKana: String,
+    val title: String,
+    val titleKana: String
 )
--- app/src/main/java/com/wings/android/bookmanagementapp/domain/search/SearchByTitleUseCaseImpl.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/domain/search/SearchByTitleUseCaseImpl.kt
@@ -4,6 +4,7 @@ import com.wings.android.bookmanagementapp.data.repository.SearchRepository
 import com.wings.android.bookmanagementapp.data.source.remote.entity.SearchResult
 import com.wings.android.bookmanagementapp.extension.toHttps
 import com.wings.android.bookmanagementapp.view.model.Book
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
@@ -13,6 +14,7 @@ class SearchByTitleUseCaseImpl @Inject constructor(
     private val repository: SearchRepository
 ) : SearchByTitleUseCase {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun invoke(title: String, page: Int): Flow<List<Book>> {
         return repository.searchByTitle(
             title = title,
@@ -28,12 +30,12 @@ class SearchByTitleUseCaseImpl @Inject constructor(
     private fun List<SearchResult>.toBookList(): List<Book> {
         return this.map { searchResult ->
             Book(
-                title = searchResult.item.title,
-                author = searchResult.item.author,
-                imageUrl = searchResult.item.largeImageUrl.toHttps(),
-                isbn = searchResult.item.isbn,
-                size = searchResult.item.size,
-                caption = searchResult.item.itemCaption
+                title = searchResult.title,
+                author = searchResult.author,
+                imageUrl = searchResult.largeImageUrl.toHttps(),
+                isbn = searchResult.isbn,
+                size = searchResult.size,
+                caption = searchResult.itemCaption
             )
         }
     }
