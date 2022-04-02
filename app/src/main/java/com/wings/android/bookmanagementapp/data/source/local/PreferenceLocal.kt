f6c53f1b724509d85544388a05f77f31e9fdaa82
ikeshoo <jyfk776@gmail.com>
2022-04-02 23:19:43 +0900

    PreferenceLocalを追加

--- app/src/main/java/com/wings/android/bookmanagementapp/data/source/local/PreferenceLocal.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/data/source/local/PreferenceLocal.kt
@@ -1,4 +1,12 @@
 package com.wings.android.bookmanagementapp.data.source.local
 
+import com.wings.android.bookmanagementapp.util.Signal
+import com.wings.android.bookmanagementapp.view.model.BookList
+import kotlinx.coroutines.flow.Flow
+
 interface PreferenceLocal {
+
+    fun getBookList(): Flow<BookList>
+
+    fun setBookList(new: BookList): Flow<Signal>
 }
---
+++ app/src/main/java/com/wings/android/bookmanagementapp/util/Signal.kt
@@ -0,0 +1,3 @@
+package com.wings.android.bookmanagementapp.util
+
+object Signal
