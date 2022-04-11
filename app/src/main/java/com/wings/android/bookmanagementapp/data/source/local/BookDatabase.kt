b6d994c3bf8f1b3e8f02e35b77d11ba214c49e5c
ikeshoo <jyfk776@gmail.com>
2022-04-11 12:37:18 +0900

    exportSchemaを追加

--- app/src/main/java/com/wings/android/bookmanagementapp/data/source/local/BookDatabase.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/data/source/local/BookDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.wings.android.bookmanagementapp.view.model.Book
 
-@Database(entities = [Book::class], version = 1)
+@Database(entities = [Book::class], version = 1, exportSchema = false)
 abstract class BookDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
 }
