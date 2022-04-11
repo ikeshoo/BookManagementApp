bd1526972b0fd047706c304d9b6a079c66fb18f1
ikeshoo <jyfk776@gmail.com>
2022-04-11 12:30:23 +0900

    DataStoreを削除

--- app/src/main/java/com/wings/android/bookmanagementapp/di/DataModule.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/di/DataModule.kt
@@ -1,17 +1,11 @@
 package com.wings.android.bookmanagementapp.di
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.dataStore
 import androidx.room.Room
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.wings.android.bookmanagementapp.data.source.local.BookDao
 import com.wings.android.bookmanagementapp.data.source.local.BookDatabase
-import com.wings.android.bookmanagementapp.data.source.local.PreferenceLocal
-import com.wings.android.bookmanagementapp.data.source.local.PreferenceLocalImpl
 import com.wings.android.bookmanagementapp.data.source.remote.RaktenApi
-import com.wings.android.bookmanagementapp.util.BookListSerializer
-import com.wings.android.bookmanagementapp.view.model.BookList
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,25 +21,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DataModule {
-
-    // DataStoreの設定
-    private const val JSON_FILE = "book_list.json"
-    private val Context.bookDataStore: DataStore<BookList> by dataStore(
-        fileName = JSON_FILE,
-        serializer = BookListSerializer
-    )
-
-    @Singleton
-    @Provides
-    fun provideDataStore(@ApplicationContext context: Context): DataStore<BookList> {
-        return context.bookDataStore
-    }
-
-    @Singleton
-    @Provides
-    fun providePreferenceLocal(dataStore: DataStore<BookList>): PreferenceLocal {
-        return PreferenceLocalImpl(dataStore)
-    }
 
     @Singleton
     @Provides
