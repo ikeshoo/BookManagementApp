80becf0fc866bc7dca24830e45209f05f583ea0e
ikeshoo <jyfk776@gmail.com>
2022-04-25 23:34:57 +0900

    詳細画面のレイアウト修正

--- app/src/main/java/com/wings/android/bookmanagementapp/view/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/wings/android/bookmanagementapp/view/ui/detail/DetailScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.FloatingActionButton
@@ -11,12 +12,14 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -38,16 +41,18 @@ fun DetailScreen(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(horizontal = 16.dp)
     ) {
+        val centerModifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         Image(
-            modifier = Modifier
+            modifier = centerModifier
                 .height(200.dp)
                 .width(200.dp),
             painter = rememberAsyncImagePainter(book.imageUrl),
             contentDescription = null
         )
-        Text(text = book.title)
-        Text(text = book.author)
+        DetailText(modifier = centerModifier, text = book.title)
+        DetailText(modifier = centerModifier, text = book.author)
         FloatingActionButton(
             modifier = Modifier.align(alignment = Alignment.End),
             onClick = {
@@ -57,4 +62,18 @@ fun DetailScreen(
             Icon(imageVector = Icons.Filled.Add, contentDescription = "")
         }
     }
+}
+
+@Composable
+fun DetailText(
+    modifier: Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier.padding(top = 8.dp),
+        text = text,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 2,
+        style = MaterialTheme.typography.titleMedium,
+    )
 }
